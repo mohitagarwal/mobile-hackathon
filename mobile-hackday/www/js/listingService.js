d7b4ff4659d22db686d68f8ae21c9f186cbc98d5
abhik.mitra <abhik.mitra@flipkart.com>
2014-08-08 01:05:44 +0530

    Merge branch 'master' of https://github.com/mohitagarwal/mobile-hackathon
    Conflicts:
    	mobile-hackday/www/index.html
    	mobile-hackday/www/js/app.js

--- mobile-hackday/www/js/listingService.js
+++ mobile-hackday/www/js/listingService.js
@@ -5,7 +5,12 @@
 angular.module('starter')
     .service('listingsFactory', ['$scope', '$firebase', function($scope, $firebase) {
         this.getListings = function(){
-
+            return {
+                "L1111" :{
+                    price : 100,
+                    image: "img.png"
+                }
+            }
         }
 
     }]);
