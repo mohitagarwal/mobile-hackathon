8caf2b9761f41246924b08575ba6fbd707f63b7e
abhik.mitra <abhik.mitra@flipkart.com>
2014-08-08 01:59:29 +0530

    Added Listing Service

--- mobile-hackday/www/js/listingService.js
+++ mobile-hackday/www/js/listingService.js
@@ -3,25 +3,24 @@
  */
 
 angular.module('starter')
-    .service('listingsFactory', ['$scope', '$firebase','$q', function($scope, $firebase, $q) {
+    .service('listingsFactory', [ '$firebase','$q', function( $firebase, $q) {
         var ref = new Firebase("https://luminous-fire-8360.firebaseio.com/");
-        var listingsRef = ref.child("listings");
+        var listingsRef = ref.child("Listings");
         var sync = $firebase(listingsRef);
         var listings = sync.$asObject();
-        var deferred = $q.defer;
-        listings.$loaded().then(function() {
+        var deferred = $q.defer();
+        listings.$loaded().then(function(data) {
+            var arr = {};
+            _.each(data, function(data,key){
+                if(key.indexOf("$") === -1){
+                    arr[key] = data;
+                }
 
-            deferred.resolve();
+            });
+            deferred.resolve(arr);
         });
         this.getListingsForCustomer = function(){
-            return deferred;
-            return {
-                "L1111" :{
-                    price : 100,
-                    image: "img.png",
-                    name: "Sabzi"
-                }
-            }
+            return deferred.promise;
         }
 
     }]);
