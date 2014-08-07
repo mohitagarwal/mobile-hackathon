04e30fe70ecdd9ed15b06b428b623832f85270d4
abhik.mitra <abhik.mitra@flipkart.com>
2014-08-08 01:21:34 +0530

    Merge branch 'master' of https://github.com/mohitagarwal/mobile-hackathon
    Conflicts:
    	mobile-hackday/www/index.html
    	mobile-hackday/www/js/app.js

--- mobile-hackday/www/js/listingService.js
+++ mobile-hackday/www/js/listingService.js
@@ -3,8 +3,18 @@
  */
 
 angular.module('starter')
-    .service('listingsFactory', ['$scope', '$firebase', function($scope, $firebase) {
+    .service('listingsFactory', ['$scope', '$firebase','$q', function($scope, $firebase, $q) {
+        var ref = new Firebase("https://luminous-fire-8360.firebaseio.com/");
+        var listingsRef = ref.child("listings");
+        var sync = $firebase(listingsRef);
+        var listings = sync.$asObject();
+        var deferred = $q.defer;
+        listings.$loaded().then(function() {
+
+            deferred.resolve();
+        });
         this.getListingsForCustomer = function(){
+            return deferred;
             return {
                 "L1111" :{
                     price : 100,
